835e6c3b047c2ef74c9f783e7aabe86ae777b7db
Jeremy Orozco Luna <49690644+JeremyOL@users.noreply.github.com>
2023-06-04 19:37:05 -0700

    [add] session?

--- movies_api/auth.py
+++ movies_api/auth.py
@@ -1,7 +1,7 @@
 import functools
 
 from flask import (
-    Blueprint, g, request, session, jsonify
+    Blueprint, g, redirect, request, session, jsonify, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -61,6 +61,8 @@ def login():
             error = 'Incorrect password'
         
     if error is None:
+        session.clear()
+        session['user_id'] = user['id']
         return jsonify({
             'status' : 'ok',
             'message' : 'login success',
@@ -71,3 +73,24 @@ def login():
         'status': 'bad',
         'message': error
     })
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
