d631b8e40fd32a22eefecbe75ccb61db41dcb32e
Jeremy Orozco Luna <49690644+JeremyOL@users.noreply.github.com>
2023-06-06 22:19:45 -0700

    app secret set and user movies bp

--- movies_api/__init__.py
+++ movies_api/__init__.py
@@ -5,6 +5,10 @@ from flask import Flask, jsonify, redirect, url_for
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
+    app.config.from_mapping(
+        SECRET_KEY = os.getenv('SECRET_KEY')
+    )
+
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
@@ -35,5 +39,9 @@ def create_app(test_config=None):
     from . import movies
     app.register_blueprint(movies.bp)
     app.add_url_rule('/', endpoint='movies.index')
+
+    # importing user blueprint
+    from . import user
+    app.register_blueprint(user.bp)
 
     return app
